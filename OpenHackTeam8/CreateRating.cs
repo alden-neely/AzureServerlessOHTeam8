cc9d82bc9433e65783f0ef40e82ba83db654b2f6
Neely Alden <Alden.Neely@hcahealthcare.com>
2020-11-17 17:28:19 -0600

    First commit, Create Rating

---
+++ OpenHackTeam8/CreateRating.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OpenHackTeam8
+{
+    public static class CreateRating
+    {
+        [FunctionName("CreateRating")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [CosmosDB(
+                databaseName: "Starfruit",
+                collectionName: "Ratings",
+                ConnectionStringSetting = "CosmosDBConnectionString")]
+                IAsyncCollector<RatingDto> ratingOut,
+            ILogger log)
+        {
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            RatingDto rating = JsonConvert.DeserializeObject<RatingDto>(requestBody);
+
+            // Validate userId and productId
+
+            // validate rating
+            if (!(rating.Rating >= 0 && rating.Rating <= 5))
+            {
+                return new BadRequestObjectResult("rating must be a number between 0 and 5");
+            }
+
+            // add ID guid
+            rating.Id = Guid.NewGuid();
+
+            // add timestamp
+            rating.Timestamp = DateTime.UtcNow;
+
+            await ratingOut.AddAsync(rating);
+
+            return new OkObjectResult(rating);
+        }
+    }
+}
---
+++ OpenHackTeam8/RatingDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenHackTeam8
+{
+    public class RatingDto
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public Guid ProductId { get; set; }
+        public string LocationName { get; set; }
+        public int Rating { get; set; }
+        public string UserNotes { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
