2e12b3394caac7366201432e52fe329a615c013b
Patrick Carver <lci6111@hca.corpad.net>
2020-11-17 21:31:46 -0600

    rough draft of userid and product validation

--- OpenHackTeam8/CreateRating.cs
+++ OpenHackTeam8/CreateRating.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Net;
 
 namespace OpenHackTeam8
 {
@@ -28,6 +29,20 @@ namespace OpenHackTeam8
             RatingDto rating = JsonConvert.DeserializeObject<RatingDto>(requestBody);
 
             // Validate userId and productId
+            var httpClient = new HttpClient();
+            var userIdResponse = await httpClient.GetAsync("https://serverlessohuser.trafficmanager.net/api/GetUser?userId=" + rating.UserId);
+
+            if (userIdResponse.StatusCode == HttpStatusCode.BadRequest) 
+            {
+                return new BadRequestObjectResult($"userid: {rating.UserId} is not valid");
+            }
+
+            var productIdResponse = await httpClient.GetAsync("https://serverlessohproduct.trafficmanager.net/api/GetProduct?productId=" + rating.ProductId);
+
+            if (productIdResponse.StatusCode == HttpStatusCode.BadRequest)
+            {
+                return new BadRequestObjectResult($"productid: {rating.ProductId} is not valid");
+            }
 
             // validate rating
             if (IsRatingRangeNotValid(rating.Rating))
@@ -49,5 +64,6 @@ namespace OpenHackTeam8
         public static bool IsRatingRangeNotValid(int rating) =>
             rating < 0 && rating > 5;
         
+
     }
 }
